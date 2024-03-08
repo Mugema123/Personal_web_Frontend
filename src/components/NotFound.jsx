c020027fe0b99c46b4794d6f3c72e40f22974be7
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2024-03-08 19:18:50 +0200

    some changes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,18 +1,20 @@
-import { Link } from 'react-router-dom';
-import { FaExclamationTriangle } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
   return (
-    <div className="w-[80%] sm:w-[65%] md:w-1/2 mx-auto flex flex-col items-center text-center space-y-4 my-8 px-4 py-8 rounded-3xl border">
-      <FaExclamationTriangle className="text-3xl text-slate-500" />
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-[80%] sm:w-[65%] md:w-1/2 mx-auto flex flex-col items-center text-center space-y-4 my-8 px-4 py-8 rounded-3xl border">
+        <FaExclamationTriangle className="text-3xl text-slate-500" />
 
-      <p>The page you're looking for is not available</p>
-      <Link
-        to="/"
-        className="text-xs bg-green text-white rounded-full px-4 py-2 shadow-lg hover:shadow-none"
-      >
-        Go to Home
-      </Link>
+        <p>The page you're looking for is not available</p>
+        <Link
+          to="/"
+          className="text-xs bg-cyan-600 text-white rounded-full px-4 py-2 shadow-lg hover:shadow-none"
+        >
+          Go to Home
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import aboutImg from "../../assets/images/mugema.jpg";
+import { useFetcher } from "../../api";
+import DataChecker from "../global/DataChecker";
+
 const About = () => {
+  const {
+    data,
+    isLoading,
+    isError,
+  } = useFetcher("/about/getAboutContent");
   const info = [
     { text: "Years experience", count: "04" },
     { text: "Completed Projects", count: "24" },
@@ -13,36 +21,45 @@ const About = () => {
           About <span className="text-cyan-600">Me</span>
         </h3>
         <p className="text-gray-400 my-3 text-lg">My introduction</p>
+        <DataChecker
+        title="About Content"
+        isLoading={isLoading}
+        isError={isError}
+        isWhiteMode={true}
+        isEmpty={!isLoading && !isError && data?.aboutContent?.length === 0}
+      >
         <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
           <div className="p-2">
             <div className="text-gray-300 my-3">
               <p className="text-justify leading-7 md:w-11/12 mx-auto">
-                I am a highly skilled software developer, deep learning
-                developer, and data scientist with a Master's degree in
-                Electrical and Computer Engineering from Carnegie Mellon
-                University. I have extensive experience working on various
-                projects in these fields, and I am confident in my ability to
-                provide valuable contributions to any team. My educational
-                background in Electrical and Computer Engineering, along with my
-                hands-on experience in the industry, has provided me with a
-                strong foundation in the latest technologies and methodologies.
-                I am a dedicated and results-driven professional with a passion
-                for developing innovative solutions to complex problems.
+                {data?.aboutContent?.[0]?.description}
               </p>
               <div className="flex mt-10 items-center gap-7">
-                {info.map((content) => (
-                  <div key={content.text}>
+                  <div>
                     <h3 className="md:text-4xl text-2xl font-semibold text-white">
-                      {content.count}
+                      {data?.aboutContent?.[0]?.yearsOfExperience}
                       <span className="text-cyan-600">+</span>{" "}
                     </h3>
-                    <span className="md:text-base text-xs">{content.text}</span>
+                    <span className="md:text-base text-xs">Years experience</span>
                   </div>
-                ))}
+                  <div>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                      {data?.aboutContent?.[0]?.completedProjects}
+                      <span className="text-cyan-600">+</span>{" "}
+                    </h3>
+                    <span className="md:text-base text-xs">Completed Projects</span>
+                  </div>
+                  <div>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                      {data?.aboutContent?.[0]?.companiesWork}
+                      <span className="text-cyan-600">+</span>{" "}
+                    </h3>
+                    <span className="md:text-base text-xs">Companies Work</span>
+                  </div>
               </div>
               <br />
               <br />
-              <a href="./src/assets/MugemaCV.pdf" download>
+              <a href={data?.aboutContent?.[0]?.cv} target="__blank" download>
                 <button className="btn-primary">Download CV</button>
               </a>
             </div>
@@ -50,11 +67,12 @@ const About = () => {
           <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
             <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
               <div className="about__me-image">
-                <img src={aboutImg} alt="Steve Ndicunguye" />
+                <img src={data?.aboutContent?.[0]?.image} alt="Mugema" />
               </div>
             </div>
           </div>
         </div>
+        </DataChecker>
       </div>
     </section>
   );
